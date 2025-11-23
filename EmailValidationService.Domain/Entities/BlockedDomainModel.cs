900e09dd8993ca7b9886befc100a2992b5a89021
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-11-23 15:33:37 -0300

    feat: cria entidade de dominio do serviço

---
+++ EmailValidationService.Domain/Entities/BlockedDomainModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EmailValidationService.Domain.Entities;
+
+public class BlockedDomainModel
+{
+    public Guid Id { get; set; }
+    public string DomainName { get; set; }
+    public DateTime DateAdded { get; set; }
+}
