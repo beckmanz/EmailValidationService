cbc61aec942ecadfa358b45722ac64527541ef3a
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-11-24 11:40:53 -0300

    feat: cria context do banco de dados

---
+++ EmailValidationService.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmailValidationService.Domain.Entities;
+
+namespace EmailValidationService.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    {
+        
+    }
+
+    public DbSet<BlockedDomainModel> BlockedDomains{ get; set; }
+}
