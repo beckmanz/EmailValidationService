a1dc85caf84dd7a8c606576b69e6509ad9a663cb
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-11-24 11:42:09 -0300

    feat: configura arquivo de injeção de dependencia

--- EmailValidationService.Infrastructure/DependencyInjection.cs
+++ EmailValidationService.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,22 @@
-﻿namespace EmailValidationService.Infrastructure;
+﻿using EmailValidationService.Application.Interfaces;
+using EmailValidationService.Infrastructure.Data;
+using EmailValidationService.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-public class DependencyInjection
+namespace EmailValidationService.Infrastructure;
+
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+        });
+        
+        services.AddScoped<IBlockedDomainRepository, BlockedDomainRepository>();
+        return services;
+    }
 }
