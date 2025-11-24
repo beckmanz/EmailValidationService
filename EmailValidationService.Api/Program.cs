9191a876e5cbc76cf3c085373690bd07a70acd90
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-11-24 12:23:42 -0300

    feat: faz a DI dos services de Infrastructure e Application e configura scalar para futura documentação do serviço

--- EmailValidationService.Api/Program.cs
+++ EmailValidationService.Api/Program.cs
@@ -1,3 +1,6 @@
+using EmailValidationService.Application;
+using EmailValidationService.Infrastructure;
+using Scalar.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +9,17 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
