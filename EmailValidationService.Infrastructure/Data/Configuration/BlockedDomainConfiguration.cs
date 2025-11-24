aa058527bc69a47c9d0778e1b257505cf64d55f0
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-11-24 12:19:51 -0300

    feat: configura propriedades da entidade de BlockedDomain

---
+++ EmailValidationService.Infrastructure/Data/Configuration/BlockedDomainConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using EmailValidationService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EmailValidationService.Infrastructure.Data.Configuration;
+
+public class BlockedDomainConfiguration : IEntityTypeConfiguration<BlockedDomainModel>
+{
+    public void Configure(EntityTypeBuilder<BlockedDomainModel> builder)
+    {
+        builder.ToTable("BlockedDomains");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.DomainName)
+            .IsRequired()
+            .HasMaxLength(200);
+                
+        builder.HasIndex(x => x.DomainName)
+            .IsUnique();
+    }
+}
