349a5d4d530211bf295f04e4120ea344a226c210
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-11-24 12:21:46 -0300

    feat: cria construtor para a entidade de BlockedDomain

--- EmailValidationService.Domain/Entities/BlockedDomainModel.cs
+++ EmailValidationService.Domain/Entities/BlockedDomainModel.cs
@@ -5,4 +5,11 @@ public class BlockedDomainModel
     public Guid Id { get; set; }
     public string DomainName { get; set; }
     public DateTime DateAdded { get; set; }
+
+    public BlockedDomainModel(string domainName)
+    {
+        Id  = Guid.NewGuid();
+        this.DomainName = domainName;
+        DateAdded = DateTime.UtcNow;
+    }
 }
